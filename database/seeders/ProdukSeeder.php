6bd3113af3205d43fd53d40da5fb19830fed16a2
MochRizkhyAKbar <mochrizkhyakbar@gmail.com>
2025-01-10 14:30:14 +0700

    Menambahkan 6 produk

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -32,6 +32,36 @@ class ProdukSeeder extends Seeder
                 'deskripsi' => 'Snack cemilan mantap, 1 bungkus 10g',
                 'harga' => 2000.00,                
             ],
+            [
+                'nama' => 'Ultra Milk',
+                'deskripsi' => 'Minuman susu sehat, 200mil',
+                'harga' => 3500.00,                
+            ],
+            [
+                'nama' => 'Sampo Clear',
+                'deskripsi' => 'Sampo pembasmi ketombe, 1 renceng',
+                'harga' => 6000.00,                
+            ],
+            [
+                'nama' => 'Minyak 1Liter',
+                'deskripsi' => 'Minyak berkualitas tinggi',
+                'harga' => 19000.00,                
+            ],
+            [
+                'nama' => 'Buah Anggur',
+                'deskripsi' => 'Anggur muscat tanpa biji, 1Kg',
+                'harga' => 45000.00,                
+            ],
+            [
+                'nama' => 'Buah Mangga',
+                'deskripsi' => 'Mangga arum manis, 1kg',
+                'harga' => 22000.00,                
+            ],
+            [
+                'nama' => 'Roti',
+                'deskripsi' => 'Roti keju, satuan',
+                'harga' => 4000.00,                 
+            ],
         ];
 
         foreach ($produks as $produk) {
