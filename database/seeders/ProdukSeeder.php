dde69b3521ed2d0eb1c8cbaaa1c6502613793128
MochRizkhyAKbar <mochrizkhyakbar@gmail.com>
2025-01-10 14:26:21 +0700

    Menambahkan 3 produk

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -17,6 +17,21 @@ class ProdukSeeder extends Seeder
                 'deskripsi' => 'Kripik jagung gurih',
                 'harga' => 3000.00,                 
             ],
+            [
+                'nama' => 'Beng-beng',
+                'deskripsi' => 'Snack coklat',
+                'harga' => 2000.00,                
+            ],
+            [
+                'nama' => 'Milku',
+                'deskripsi' => 'Minuman susu berkualitas',
+                'harga' => 3500.00,                
+            ],
+            [
+                'nama' => 'Taro',
+                'deskripsi' => 'Snack cemilan mantap, 1 bungkus 10g',
+                'harga' => 2000.00,                
+            ],
         ];
 
         foreach ($produks as $produk) {
